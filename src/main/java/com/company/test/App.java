2bd3b6cc61e307c8d6bcb5c045811ccf62aa907d
Agustin Martinez <agustin.martinez@sonaype.com>
2022-12-20 12:26:01 -0500

    trigger errorprone

--- src/main/java/com/company/test/App.java
+++ src/main/java/com/company/test/App.java
@@ -4,9 +4,13 @@ class App {
     public static void main(String[] args) {
         infiniteRecursion(99);
         System.out.println(NANOS_PER_DAY);
+        compare(new String[]{"this"}, new String[]{"that"});
     }
 
     static int infiniteRecursion(int x ){
         return infiniteRecursion(x+1);
+    }
+    static boolean compare(String firstList[], String secondList[]) {
+        return firstList.equals(secondList);
     }
 }
