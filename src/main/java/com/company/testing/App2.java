de42cc8b154fa5494235e6d4562cf2320f12c7ec
Agustin Martinez <108492518+agustin-mrtz@users.noreply.github.com>
2023-01-05 17:00:55 -0500

    Create App2.java

---
+++ src/main/java/com/company/testing/App2.java
@@ -0,0 +1,61 @@
+class App2 {
+    static final long NANOS_PER_DAY = 24  * 60 * 60 * 1000 * 1000 * 1000;
+    static final long NANOS_PER_DAY = 24  * 60 * 60 * 1000 * 1000 * 2000;
+    static String username = "AGUSTIN";
+    static String USERNAME = "AGUSTIN";
+
+    public static void main(String[] args) {
+        infiniteRecursion(99);
+        infiniteRecursion(100);
+        baseCase(23);
+        System.out.println(NANOS_PER_DAY);
+        compare(new String[]{"this"}, new String[]{"that"});
+        compare(new String[]{"this2"}, new String[]{"that2"});
+    }
+
+    static int infiniteRecursion(int x ){
+        return infiniteRecursion(x+1);
+    }
+    
+    static int baseCase(int x ){
+        return baseCase(x+1);
+    }
+    
+    static boolean compare(String firstList[], String secondList[]) {
+        return firstList.equals(secondList);
+    }
+
+    static class Data1
+    {
+        private int a;
+
+        @Override
+        public boolean equals(Object other) {
+            Data1 that = (Data1) other; // BAD: This may throw ClassCastException.
+            return a == that.a;
+        }
+    }
+
+
+    static class Data2
+    {
+        private int b;
+
+        @Override
+        public boolean equals(Object other) {
+            Data2 that = (Data2) other; // BAD: This may throw ClassCastException.
+            return b == that.b;
+        }
+    }
+    
+      static class Data3
+    {
+        private int b;
+
+        @Override
+        public boolean equals(Object other) {
+            Data3 that = (Data3) other; // BAD: This may throw ClassCastException.
+            return b == that.b;
+        }
+    }
+}
